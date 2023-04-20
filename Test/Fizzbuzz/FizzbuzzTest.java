8247bfca4eac5e5155d87f5d9fe1ae974f518214
Solene Frances-gentillet <frs4440a@ens.fsi>
2023-04-20 17:02:15 +0200

    Reponse correcte sur 6 et 400

--- Test/Fizzbuzz/FizzbuzzTest.java
+++ Test/Fizzbuzz/FizzbuzzTest.java
@@ -28,5 +28,24 @@ class FizzbuzzTest {
 	void fizzbuzz_repond_3_pour_3() {
 		assertEquals("fizz",fb.fuzzBuzz(3));	
 	}
+	
+	@Test
+	void fizzbuzz_repond_un_entier() {
+		assertEquals("4",fb.fuzzBuzz(4));
+		assertEquals("7",fb.fuzzBuzz(7));	
+		assertEquals("13",fb.fuzzBuzz(13));		
+		
+	}
+	
+	@Test
+	void fizzbuzz_repond_buzz() {
+		assertEquals("buzz",fb.fuzzBuzz(5));	
+	}
+	
+	@Test
+	void fizzbuzz_repond_correctement_sur_multiple() {
+		assertEquals("fizz",fb.fuzzBuzz(6));
+		assertEquals("buzz",fb.fuzzBuzz(400));
+	}
 
 }
--- src/Fizzbuzz/Fizzbuzz.java
+++ src/Fizzbuzz/Fizzbuzz.java
@@ -1,12 +1,17 @@
 package Fizzbuzz;
 
 public class Fizzbuzz {
+	
+	private static final int[] numSpeciaux= {3,5};
+	private static final String[] traduction= {"fizz","buzz"}; 
 
-	public String fuzzBuzz(int i) {
-		if (i==3) {
-			return "fizz";
+	public String fuzzBuzz(int numero) {
+		for (int i=0;i<numSpeciaux.length;i++) {
+			if (numero%numSpeciaux[i]==0) {
+				return traduction[i];
+			}
 		}
-		return Integer.toString(i);
+		return Integer.toString(numero);
 		
 	}
 }
